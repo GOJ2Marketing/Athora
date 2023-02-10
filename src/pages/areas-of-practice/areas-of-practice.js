caca55c191a4c8ffa3208270a013bf784a4e8d89
Devlin <jacobbrewerrooney@gmail.com>
2023-02-10 14:56:59 -0500

    areas of practice much better

--- src/pages/areas-of-practice/areas-of-practice.js
+++ src/pages/areas-of-practice/areas-of-practice.js
@@ -27,41 +27,36 @@ export default function Areas() {
             __html: description,
           }}
         /> */}
-        <Container>
           <h1>Areas of Practice</h1>
-          <p className={areas.subtitleDesk}>
+          <h2 className={areas.subtitleDesk}>
             Working with our team through any of our practice areas will provide you with unmatched service and care. We
             work together across our practice areas as a team to provide the best results for our clients.
-          </p>
-        </Container>
+          </h2>
         <div className={styles.scrollDown}>
           <p>Scroll</p>
           <div></div>
         </div>
       </Header>
-
-      <Section>
         <Container>
           <p className={areas.subtitleMobile}>
             Working with our team through any of our practice areas will provide you with unmatched service and care. We
             work together across our practice areas as a team to provide the best results for our clients.
           </p>
         </Container>
-      </Section>
       <Section className={areas.cardContainer}>
         <CardContainer>
-          <HoverCard img="/businessLaw.png" title1="Business" title2="Law" desc="A business is more than just how you support yourself. We provide guidance for many practice areas, from employment law to specific nuances of business industries. We have extensive experience from beginning to end and can confidently assist you in any business endeavor."/>
-          <HoverCard img="/estateLaw.png" title1="Estate Planning &" title2="Administration" desc="Whether you’re looking to protect your assets or decide how you’d like to proceed if you become impaired, Estate Planning encompasses it all. Most people associate estate planning with just a will, but there’s more to do in order to plan for every situation. We are experienced in comprehensive estate planning & administration and can help protect you and your loved ones."/>
-          <HoverCard img="/litigationLaw.png" title1="Commercial" title2="Litigation" desc="Whether you’re looking to protect your assets or decide how you’d like to proceed if you become impaired, Estate Planning encompasses it all. Most people associate estate planning with just a will, but there’s more to do in order to plan for every situation. We are experienced in comprehensive estate planning & administration and can help protect you and your loved ones."/>
+          <HoverCard img="/businessLaw.png" title1="Business" title2="Law" button="Read More" desc="A business is more than just how you support yourself. We provide guidance for many practice areas, from employment law to specific nuances of business industries. We have extensive experience from beginning to end and can confidently assist you in any business endeavor."/>
+          <HoverCard img="/estateLaw.png" title1="Estate Planning &" title2="Administration" button="Read More" desc="Whether you’re looking to protect your assets or decide how you’d like to proceed if you become impaired, Estate Planning encompasses it all. Most people associate estate planning with just a will, but there’s more to do in order to plan for every situation. We are experienced in comprehensive estate planning & administration and can help protect you and your loved ones."/>
+          <HoverCard img="/litigationLaw.png" title1="Commercial" title2="Litigation" button="Read More" desc="Whether you’re looking to protect your assets or decide how you’d like to proceed if you become impaired, Estate Planning encompasses it all. Most people associate estate planning with just a will, but there’s more to do in order to plan for every situation. We are experienced in comprehensive estate planning & administration and can help protect you and your loved ones."/>
         </CardContainer>
-        <Container className={areas.disclaimerContainer}>
+      </Section>
+      <Container className={areas.disclaimerContainer}>
           <p className={areas.disclaimer}>
             The information on this website is for general information purposes only. Nothing on this site should be
             considered legal advice for any individual case or situation. This information does not constitute an
             attorney-client relationship.
           </p>
-        </Container>
-      </Section>
+      </Container>
 
       {/* <Section>
         <Container>
