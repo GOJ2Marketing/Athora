4fcaae6595fb0e6c9a7556d2da373e9f35b8463c
Devlin <jacobbrewerrooney@gmail.com>
2023-02-15 14:08:00 -0500

    practice page components slap

--- src/components/Practice/PracticeFAQ/FAQ.js
+++ src/components/Practice/PracticeFAQ/FAQ.js
@@ -1,34 +1,55 @@
 import Container from 'components/Container';
 import * as Accordion from '@radix-ui/react-accordion';
+
 import Style from './FAQ.module.scss';
 
-const PracticeHead = () => {
+const PracticeHead = ({ list }) => {
   return (
-    <div className={Style.container}>
-      <h1>FAQ</h1>
-      <Container className={Style.contentContainer}>
-        <Accordion.Root className={Style.accordionRoot} type="single" defaultValue="item1" collapsible>
-          <Accordion.Item className={Style.accordionItem} value="item1">
-            <Accordion.Header>
-              <Accordion.Trigger>I want to retire, but what do I do with my business?</Accordion.Trigger>
-            </Accordion.Header>
-            <Accordion.Content className={Style.accordionContent}>
-              You’ve spent years building your business. But now you’re looking forward to stepping back and letting
-              someone else take the reins. Whether that is one or more of your children, another family member or a
-              trusted employee, you will need to create contracts that control who will own what interests in the
-              business, when and how they will receive them. And if you want to benefit all of your children, but not
-              all are involved in the business, it will be important to consider a distinction between voting and
-              non-voting shares, or perhaps some different classes of shares, in order to ensure good relationships
-              between your children after you step-away. Keep in mind it’s not enough to plan for how family members or
-              key employees will step up when you retire; you also need to plan how you will step away. The years
-              leading up to your retirement should be a learning experience that allows you time to train your
-              successors on the company’s management. Both are key components to the business succession plan Athora Law
-              can assist you with.{' '}
-            </Accordion.Content>
-          </Accordion.Item>
-        </Accordion.Root>
-      </Container>
-    </div>
+    <>
+      <div className={Style.container}>
+        <h1>FAQ</h1>
+        <Container className={Style.contentContainer}>
+          <Accordion.Root className={Style.accordionRoot} type="single" collapsible>
+            {list.map((e) => {
+              return (
+                <Accordion.Item key={e.tab} className={Style.accordionItem} value={e.tab}>
+                  <Accordion.Header>
+                    <Accordion.Trigger className={Style.trigger}>
+                      <h3>{e.title}</h3>
+                      <div className={Style.svg}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="3" viewBox="0 0 28 3">
+                          <path
+                            id="remove_FILL0_wght400_GRAD0_opsz48"
+                            d="M10,25.5v-3H38v3Z"
+                            transform="translate(-10 -22.5)"
+                            fill="#352540"
+                          />
+                        </svg>
+                        <svg
+                          className={Style.vert}
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="28"
+                          height="3"
+                          viewBox="0 0 28 3"
+                        >
+                          <path
+                            id="remove_FILL0_wght400_GRAD0_opsz48"
+                            d="M10,25.5v-3H38v3Z"
+                            transform="translate(-10 -22.5)"
+                            fill="#352540"
+                          />
+                        </svg>
+                      </div>
+                    </Accordion.Trigger>
+                  </Accordion.Header>
+                  <Accordion.Content className={Style.accordionContent}>{e.desc} </Accordion.Content>
+                </Accordion.Item>
+              );
+            })}
+          </Accordion.Root>
+        </Container>
+      </div>
+    </>
   );
 };
 
--- src/pages/business-law/business-law.js
+++ src/pages/business-law/business-law.js
@@ -9,48 +9,6 @@ import CTA from 'components/Practice/CTA';
 import PracticeFullImg from 'components/Practice/PracticeFullImg';
 import PracticeFAQ from 'components/Practice/PracticeFAQ';
 
-const industryList = [
-  {
-    title: 'Construction Law',
-    tab: 'tab1',
-    desc: 'Construction law is a combination of contract, commercial, and tort law. With the complexity of warranties, contracts, regulations, and an often-lengthy chain of contractors and sub-contractors, we know that construction law can be confusing. We represent both owners and construction firms, and we are well-versed in mechanics liens, construction defects (owners and contractors), and contractual disputes. We can work with you to clarify this process and get the resolution you need.'
-  },
-  {
-    title: 'Financial Institutions',
-    tab: 'tab2',
-    desc: 'combination of contract, commercial, and tort law. With the complexity of warranties, contracts, regulations, and an often-lengthy chain of contractors and sub-contractors, we know that construction law can be confusing. We represent both owners and construction firms, and we are well-versed in mechanics liens, construction defects (owners and contractors), and contractual disputes. We can work with you to clarify this process and get the resolution you need.'
-  }
-]
-
-const helpList = [
-  {
-    title: 'Construction Litigation',
-    tab: 'tab1',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
-  },
-  {
-    title: 'Non-Complete Litigation',
-    tab: 'tab2',
-    desc: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
-  },
-  {
-    title: 'Contractual Disputes',
-    tab: 'tab3',
-    desc: 'od tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
-  },
-  {
-    title: 'Shareholder Disputes',
-    tab: 'tab4',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
-  },
-  {
-    title: 'Real Estate Disputes',
-    tab: 'tab5',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget. ut labore et dolore magna aliqua. Ut sem viverra aliquet eget. ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
-  },
-]
-
-
 export default function Areas() {
   const { metadata = {} } = useSite();
   const { title } = metadata;
@@ -63,11 +21,82 @@ export default function Areas() {
       <Header title='Business Law' img='/businessLaw.png' desc="A business is more than just how you support yourself. We can provide guidance for many practice areas, from employment law to specific nuances of business industries. We have extensive experience from beginning to end and can confidently assist you in any business endeavor."/>
 
       <PracticeTabs title='How We Can Help' list={helpList}/>
-      <PracticeTabs title='Industry Focuses' list={industryList}/>
+      <PracticeTabs title='Industry Focuses' list={highlightList}/>
 
-      <CTA />
+      <CTA title="Schedule a Consultation" button="Contact Us" background='#fff'/>
       <PracticeFullImg src="/businessImg.png"/>
-      <PracticeFAQ />
+      <PracticeFAQ list={FAQ}/>
+      <CTA title="Can't find what you're looking for?" button="FAQs" background='#e2ddd0'/>
+      <PracticeFullImg src="/businessImg.png" disclaimer="The information on this website is for general information purposes only. Nothing on this site should be considered legal advice for any individual case or situation. This information does not constitute an attorney-client relationship."/>
     </Layout>
   );
 }
+
+const helpList = [
+  {
+    title: 'Starting a Business',
+    tab: 'tab1',
+    desc: 'Construction law is a combination of contract, commercial, and tort law. With the complexity of warranties, contracts, regulations, and an often-lengthy chain of contractors and sub-contractors, we know that construction law can be confusing. We represent both owners and construction firms, and we are well-versed in mechanics liens, construction defects (owners and contractors), and contractual disputes. We can work with you to clarify this process and get the resolution you need.'
+  },
+  {
+    title: 'Buying or Selling a Business',
+    tab: 'tab2',
+    desc: 'combination of contract, commercial, and tort law. With the complexity of warranties, contracts, regulations, and an often-lengthy chain of contractors and sub-contractors, we know that construction law can be confusing. We represent both owners and construction firms, and we are well-versed in mechanics liens, construction defects (owners and contractors), and contractual disputes. We can work with you to clarify this process and get the resolution you need.'
+  },
+  {
+    title: 'Contracts',
+    tab: 'tab2',
+    desc: 'combination of contract, commercial, and tort law. With the complexity of warranties, contracts, regulations, and an often-lengthy chain of contractors and sub-contractors, we know that construction law can be confusing. We represent both owners and construction firms, and we are well-versed in mechanics liens, construction defects (owners and contractors), and contractual disputes. We can work with you to clarify this process and get the resolution you need.'
+  },
+  {
+    title: 'Succession Planning',
+    tab: 'tab2',
+    desc: 'combination of contract, commercial, and tort law. With the complexity of warranties, contracts, regulations, and an often-lengthy chain of contractors and sub-contractors, we know that construction law can be confusing. We represent both owners and construction firms, and we are well-versed in mechanics liens, construction defects (owners and contractors), and contractual disputes. We can work with you to clarify this process and get the resolution you need.'
+  }
+]
+
+const highlightList = [
+  {
+    title: 'Farming',
+    tab: 'tab1',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
+  },
+  {
+    title: 'Real Estate',
+    tab: 'tab2',
+    desc: 'consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
+  },
+  {
+    title: 'Contractual Disputes',
+    tab: 'tab3',
+    desc: 'od tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
+  },
+  {
+    title: "Creditor's Rights",
+    tab: 'tab4',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
+  },
+  {
+    title: 'Employment Law',
+    tab: 'tab5',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut sem viverra aliquet eget. ut labore et dolore magna aliqua. Ut sem viverra aliquet eget. ut labore et dolore magna aliqua. Ut sem viverra aliquet eget.'
+  },
+]
+
+const FAQ = [
+  {
+    title: 'I want to retire, but what do I do with my business?',
+    tab: 'tab1',
+    desc: 'You’ve spent years building your business. But now you’re looking forward to stepping back and letting someone else take the reins. Whether that is one or more of your children, another family member or a trusted employee, you will need to create contracts that control who will own what interests in the business, when and how they will receive them. And if you want to benefit all of your children, but not all are involved in the business, it will be important to consider a distinction between voting and non-voting shares, or perhaps some different classes of shares, in order to ensure good relationships between your children after you step-away. Keep in mind it’s not enough to plan for how family members or key employees will step up when you retire; you also need to plan how you will step away. The years leading up to your retirement should be a learning experience that allows you time to train your successors on the company’s management. Both are key components to the business succession plan Athora Law can assist you with.'
+  },
+  {
+    title: 'I want to retire, but what do I do with my business?',
+    tab: 'tab2',
+    desc: 'You’ve spent years building your business. But now you’re looking forward to stepping back and letting someone else take the reins. Whether that is one or more of your children, another family member or a trusted employee, you will need to create contracts that control who will own what interests in the business, when and how they will receive them. And if you want to benefit all of your children, but not all are involved in the business, it will be important to consider a distinction between voting and non-voting shares, or perhaps some different classes of shares, in order to ensure good relationships between your children after you step-away. Keep in mind it’s not enough to plan for how family members or key employees will step up when you retire; you also need to plan how you will step away. The years leading up to your retirement should be a learning experience that allows you time to train your successors on the company’s management. Both are key components to the business succession plan Athora Law can assist you with.'
+  },
+  {
+    title: 'I want to retire, but what do I do with my business?',
+    tab: 'tab3',
+    desc: 'You’ve spent years building your business. But now you’re looking forward to stepping back and letting someone else take the reins. Whether that is one or more of your children, another family member or a trusted employee, you will need to create contracts that control who will own what interests in the business, when and how they will receive them. And if you want to benefit all of your children, but not all are involved in the business, it will be important to consider a distinction between voting and non-voting shares, or perhaps some different classes of shares, in order to ensure good relationships between your children after you step-away. Keep in mind it’s not enough to plan for how family members or key employees will step up when you retire; you also need to plan how you will step away. The years leading up to your retirement should be a learning experience that allows you time to train your successors on the company’s management. Both are key components to the business succession plan Athora Law can assist you with.'
+  },
+]
