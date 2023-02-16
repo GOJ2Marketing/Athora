4b9dece70dd81f58a9cf3f7ab5a6030183ca8bce
Cody Grace <codyrichardgrace@gmail.com>
2023-02-16 01:26:00 -0500

    Areas of Practice

--- src/pages/areas-of-practice/areas-of-practice.js
+++ src/pages/areas-of-practice/areas-of-practice.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import useSite from 'hooks/use-site';
 import { WebsiteJsonLd } from 'lib/json-ld';
 
@@ -6,11 +5,10 @@ import Layout from 'components/Layout';
 import Header from 'components/Header';
 import Section from 'components/Section';
 import Container from 'components/Container';
-import CardContainer from 'components/CardContainer';
-import HoverCard from 'components/HoverCard';
 
 import styles from 'styles/pages/Home.module.scss';
 import areas from './Areas-of-practice.module.scss';
+import NewCardContainer from 'components/NewCardContainer';
 
 export default function Areas() {
   const { metadata = {} } = useSite();
@@ -22,65 +20,34 @@ export default function Areas() {
     <Layout>
       <WebsiteJsonLd siteTitle={title} />
       <Header>
-        {/* <h1
-          dangerouslySetInnerHTML={{
-            __html: description,
-          }}
-        /> */}
-          <h1>Areas of Practice</h1>
-          <h2 className={areas.subtitleDesk}>
-            Working with our team through any of our practice areas will provide you with unmatched service and care. We
-            work together across our practice areas as a team to provide the best results for our clients.
-          </h2>
+        <h1>Areas of Practice</h1>
+        <h2 className={areas.subtitleDesk}>
+          Working with our team through any of our practice areas will provide you with unmatched service and care. We
+          work together across our practice areas as a team to provide the best results for our clients.
+        </h2>
         <div className={styles.scrollDown}>
           <p>Scroll</p>
           <div></div>
         </div>
       </Header>
-        <Container>
-          <p className={areas.subtitleMobile}>
-            Working with our team through any of our practice areas will provide you with unmatched service and care. We
-            work together across our practice areas as a team to provide the best results for our clients.
-          </p>
-        </Container>
-      <Section className={areas.cardContainer}>
-        <CardContainer>
-          <HoverCard img="/businessLaw.png" title1="Business" title2="Law" button="Read More" desc="A business is more than just how you support yourself. We provide guidance for many practice areas, from employment law to specific nuances of business industries. We have extensive experience from beginning to end and can confidently assist you in any business endeavor."/>
-          <HoverCard img="/estateLaw.png" title1="Estate Planning &" title2="Administration" button="Read More" desc="Whether you’re looking to protect your assets or decide how you’d like to proceed if you become impaired, Estate Planning encompasses it all. Most people associate estate planning with just a will, but there’s more to do in order to plan for every situation. We are experienced in comprehensive estate planning & administration and can help protect you and your loved ones."/>
-          <HoverCard img="/litigationLaw.png" title1="Commercial" title2="Litigation" button="Read More" desc="Whether you’re looking to protect your assets or decide how you’d like to proceed if you become impaired, Estate Planning encompasses it all. Most people associate estate planning with just a will, but there’s more to do in order to plan for every situation. We are experienced in comprehensive estate planning & administration and can help protect you and your loved ones."/>
-        </CardContainer>
-      </Section>
-      <Container className={areas.disclaimerContainer}>
-          <p className={areas.disclaimer}>
-            The information on this website is for general information purposes only. Nothing on this site should be
-            considered legal advice for any individual case or situation. This information does not constitute an
-            attorney-client relationship.
-          </p>
+      <Container>
+        <p className={areas.subtitleMobile}>
+          Working with our team through any of our practice areas will provide you with unmatched service and care. We
+          work together across our practice areas as a team to provide the best results for our clients.
+        </p>
       </Container>
-
-      {/* <Section>
-        <Container>
-          <h2 className="sr-only">Posts</h2>
-          <ul className={styles.posts}>
-            {posts.map((post) => {
-              console.log(post);
-              return (
-                <li key={post.slug}>
-                  <PostCard post={post} />
-                </li>
-              );
-            })}
-          </ul>
-          {pagination && (
-            <Pagination
-              addCanonical={false}
-              currentPage={pagination?.currentPage}
-              pagesCount={pagination?.pagesCount}
-              basePath={pagination?.basePath}
-            />
-          )}
-        </Container>
-          </Section>*/}
+      <div className={styles.noPadTop}>
+        <Section>
+          <NewCardContainer />
+        </Section>
+      </div>
+      <Container className={areas.disclaimerContainer}>
+        <p className={areas.disclaimer}>
+          The information on this website is for general information purposes only. Nothing on this site should be
+          considered legal advice for any individual case or situation. This information does not constitute an
+          attorney-client relationship.
+        </p>
+      </Container>
     </Layout>
   );
 }
