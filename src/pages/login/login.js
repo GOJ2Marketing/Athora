9a5a98da55a1bf7f1b0a88feb4a62687eb02dbd1
jacob76134 <you@example.com>
2023-03-07 15:00:32 -0500

    login spicy

---
+++ src/pages/login/login.js
@@ -0,0 +1,12 @@
+import Layout from 'components/Layout';
+import WordPressLogin from 'components/WordPressLogin';
+
+const Login = () => {
+  return (
+    <Layout>
+      <WordPressLogin />
+    </Layout>
+  );
+};
+
+export default Login;
