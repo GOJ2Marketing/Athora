0c4e474575f7ac2c2a4f52e80d9b93fae39c30a0
jacob76134 <you@example.com>
2023-03-09 14:58:08 -0500

    vercel deployment fixes 3

--- src/pages/categories/[slug].js
+++ src/pages/categories/[slug].js
@@ -1,73 +1,73 @@
-import { getCategoryBySlug } from 'lib/categories';
-import { getPostsByCategoryId } from 'lib/posts';
-import usePageMetadata from 'hooks/use-page-metadata';
+// import { getCategoryBySlug } from 'lib/categories';
+// import { getPostsByCategoryId } from 'lib/posts';
+// import usePageMetadata from 'hooks/use-page-metadata';
 
-import TemplateArchive from 'templates/archive';
-import Title from 'components/Title';
+// import TemplateArchive from 'templates/archive';
+// import Title from 'components/Title';
 
-export default function Category({ category, posts }) {
-  const { name, description, slug } = category;
+// export default function Category({ category, posts }) {
+//   const { name, description, slug } = category;
 
-  const { metadata } = usePageMetadata({
-    metadata: {
-      ...category,
-      description: description || category.og?.description || `Read ${posts.length} posts from ${name}`,
-    },
-  });
+//   const { metadata } = usePageMetadata({
+//     metadata: {
+//       ...category,
+//       description: description || category.og?.description || `Read ${posts.length} posts from ${name}`,
+//     },
+//   });
 
-  return <TemplateArchive title={name} Title={<Title title={name} />} posts={posts} slug={slug} metadata={metadata} />;
-}
+//   return <TemplateArchive title={name} Title={<Title title={name} />} posts={posts} slug={slug} metadata={metadata} />;
+// }
 
-export async function getStaticProps({ params = {} } = {}) {
-  const { category } = await getCategoryBySlug(params?.slug);
+// export async function getStaticProps({ params = {} } = {}) {
+//   const { category } = await getCategoryBySlug(params?.slug);
 
-  if (!category) {
-    return {
-      props: {},
-      notFound: true,
-    };
-  }
+//   if (!category) {
+//     return {
+//       props: {},
+//       notFound: true,
+//     };
+//   }
 
-  const { posts } = await getPostsByCategoryId({
-    categoryId: category.databaseId,
-    queryIncludes: 'archive',
-  });
+//   const { posts } = await getPostsByCategoryId({
+//     categoryId: category.databaseId,
+//     queryIncludes: 'archive',
+//   });
 
-  return {
-    props: {
-      category,
-      posts,
-    },
-  };
-}
+//   return {
+//     props: {
+//       category,
+//       posts,
+//     },
+//   };
+// }
 
-export async function getStaticPaths() {
-  // By default, we don't render any Category pages as
-  // we're considering them non-critical pages
+// export async function getStaticPaths() {
+//   // By default, we don't render any Category pages as
+//   // we're considering them non-critical pages
 
-  // To enable pre-rendering of Category pages:
+//   // To enable pre-rendering of Category pages:
 
-  // 1. Add import to the top of the file
-  //
-  // import { getAllCategories, getCategoryBySlug } from 'lib/categories';
+//   // 1. Add import to the top of the file
+//   //
+//   // import { getAllCategories, getCategoryBySlug } from 'lib/categories';
 
-  // 2. Uncomment the below
-  //
-  // const { categories } = await getAllCategories();
+//   // 2. Uncomment the below
+//   //
+//   // const { categories } = await getAllCategories();
 
-  // const paths = categories.map((category) => {
-  //   const { slug } = category;
-  //   return {
-  //     params: {
-  //       slug,
-  //     },
-  //   };
-  // });
+//   // const paths = categories.map((category) => {
+//   //   const { slug } = category;
+//   //   return {
+//   //     params: {
+//   //       slug,
+//   //     },
+//   //   };
+//   // });
 
-  // 3. Update `paths` in the return statement below to reference the `paths` constant above
+//   // 3. Update `paths` in the return statement below to reference the `paths` constant above
 
-  return {
-    paths: [],
-    fallback: 'blocking',
-  };
-}
+//   return {
+//     paths: [],
+//     fallback: 'blocking',
+//   };
+// }
